e356ea658fce9fffa30f8ab9c9cac56c61fe9404
Tieen001 <134282928+Tieen001@users.noreply.github.com>
2026-06-16 00:53:34 +0700

    Fix error

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 app.use("/api/contacts", contactsRouter);
 app.use((req, res, next) => {
   // Code ở đây sẽ chạy khi không có route được định nghĩa nào
@@ -12,8 +13,8 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // Middleware xử lý lỗi tập trung.
   // Trong các đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xử lý lỗi này
-  return res.status(error.statusCode || 500).json({
-    message: error.message || "Internal Server Error",
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
   });
 });
 app.use(cors());
